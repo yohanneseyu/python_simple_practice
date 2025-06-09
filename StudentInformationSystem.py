6e5418bb6637c006208b4fd4b4c19cf55f6afdb8
Jhonny <davejhon126@gmail.com>
2025-06-09 15:59:17 +0200

    udate information on view function and delete function

--- StudentInformationSystem.py
+++ StudentInformationSystem.py
@@ -11,6 +11,18 @@ def option():
     print('6.EXIT')
 
 
+def viewoption():
+    print(f"\n ***********Hello  What do you want to do ***********")
+    print("1.For Individual Student")
+    print("2.For All Student Information")
+    return
+def deloption():
+    print(f"\n ***********Hello  What do you want to do ***********")
+    print(f"\n 1.Delete an Individual Student ")
+    print(f"\n 2.Delete All Student Information")
+    return
+
+
 def add():
     try:
         print(" ==============Here Enter All Student information ==============\n")
@@ -31,7 +43,7 @@ def add():
             "grade": student_grade
         }
 
-        print(f"\nStudent ID:- {student_id} Successfully Added ")
+        print(f"\nStudent ID: - {student_id} Successfully Added ")
 
     except ValueError as ve:
         print(f"Value Error: {ve} please check your inputs")
@@ -44,7 +56,32 @@ def add():
 
 
 def view():
-    print("VIEW code comming soon .......")
+    viewoption()
+    view_choice = input("\nEnter Your Choice :- ")
+    if view_choice == "1":
+        try:
+            studentid = input("\nEnter The Student ID :- ")
+            if studentid not in students_info:
+                print(f"\nThe ID you entered does not exist")
+                return
+            print(f"\n ********* The student with the correspond Id number {studentid} have this Info *********")
+            for key, value in students_info[studentid].items():
+                print(f"{key.capitalize()}: {value}")
+        except ValueError as e:
+            print(f"Value Error: {e} please check your inputs")
+
+    elif view_choice == "2":
+        if not students_info:
+            print(f"\n No Student Information Available")
+        else:
+            for sid, info in students_info.items():
+                print(f"\nStudent ID: {sid}")
+                for key, value in info.items():
+                    print(f"{key.capitalize()}: {value}")
+    else:
+        print(f"Invalid Choice. Please use 1 or 2")
+
+
 
 
 def search():
@@ -56,7 +93,32 @@ def edit():
 
 
 def delete():
-    print("DELETE code comming soon ......")
+    deloption()
+    delchoice = input("\nEnter Your Choice :- ")
+    try:
+        if delchoice == "1":
+            delchoice1 = input("\nEnter Student ID :- ")
+            if delchoice1 in students_info:
+                del students_info[delchoice1]
+                print(f"\nStudent ID {delchoice1} has been Deleted successfully ")
+            else:
+                print(f"\nStudent ID {delchoice1} Does not exist. please Enter correct student ID")
+        elif delchoice == "2":
+            confirmdel = input("\nAre you sure you want to delete the entire the student information, Answer with "
+                               "yes/no :- ").lower()
+            if confirmdel == "yes":
+                students_info.clear()
+                print(f"\n The Entire student information has been deleted ")
+            else:
+                print(f"\n Deletion has been Aborted")
+        else:
+            print(f"\n Enter a Valid Option. please use 1 or 2. ")
+    except Exception as e:
+        print(f"an error occurred: {e}")
+
+
+
+
 
 
 def menu():
@@ -85,6 +147,7 @@ def main():
     while True:
         first_name = input("Enter your first name :- ").strip()
         last_name = input("Enter your last name :- ").strip()
+
         if first_name.isalpha() and last_name.isalpha():
             print(f"hello {first_name.capitalize()}, {last_name.capitalize()} Wellcome back to our student "
                   f"information system")
